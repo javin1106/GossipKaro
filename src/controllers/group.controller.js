746d12d5d874df9c312428e25606ce5c48cf9fa2
Javin <javin@example.com>
2026-01-29 20:02:57 +0530

    feat: Add message retrieval and group detail fetching functionality

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -1,4 +1,5 @@
 import Group from "../models/group.model.js";
+import Message from "../models/message.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -25,3 +26,107 @@ export const createGroup = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, group, "Group created successfully"));
 });
 
+export const getMessages = asyncHandler(async (req, res) => {
+  const { groupId } = req.params;
+  const userId = req.user._id;
+
+  if (!groupId) {
+    throw new ApiError(400, "Group ID is required");
+  }
+
+  const group = await Group.findById(groupId);
+
+  if (!group) {
+    throw new ApiError(404, "Group not found");
+  }
+
+  const isMember = group.members.some(
+    (memberId) => memberId.toString() === userId.toString(),
+  );
+
+  if (!isMember) {
+    throw new ApiError(403, "You are not a member of this group");
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 20;
+  const skip = (page - 1) * limit;
+
+  // Fetch all messages
+  const messages = await Message.find({ group: groupId })
+    .sort({
+      createdAt: -1,
+    })
+    .skip(skip)
+    .limit(limit)
+    .populate("sender", "username email");
+
+  const totalMessages = await Message.countDocuments({ group: groupId });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        messages: messages,
+        pagination: {
+          page,
+
+          limit,
+          total: totalMessages,
+          totalPages: Math.ceil(totalMessages / limit),
+        },
+      },
+      "Messages retreived successfully",
+    ),
+  );
+});
+
+export const getGroupDetails = asyncHandler(async (req, res) => {
+  const groupId = req.params;
+  const userId = req.user._id;
+  if (!groupId) throw new ApiError(400, "Group ID is required");
+
+  const group = await Group.findById(groupId).populate("members", "username");
+
+  if (!group) {
+    throw new ApiError(404, "Group not found");
+  }
+
+  const isMember = group.members.some(
+    (memberId) => memberId.toString() === userId.toString(),
+  );
+
+  if (!isMember) {
+    throw new ApiError(403, "You are not a member of this group");
+  }
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        group,
+      },
+      "Group details fetched successfully",
+    ),
+  );
+});
+
+export const getUserGroups = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  if (!userId) {
+    throw new ApiError(400, "User ID is required");
+  }
+
+  const groups = await Group.find({
+    members: userId,
+  })
+    .populate("members", "username email")
+    .populate("admins", "username email")
+    .sort({ updatedAt: -1 });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, groups, "User's groups fetched successfully"));
+});
